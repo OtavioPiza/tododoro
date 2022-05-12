95a2ef1ca3476347ed7e8081b833c270d805f7ce
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2022-05-11 23:14:16 -0300

    refactor

--- routes/auth.js
+++ routes/auth.js
@@ -1,179 +1,128 @@
-/**
- * router for auth routes
- *
- * @version 1.0.0
- */
-
-// == imports == //
-
-/* libs */
-
 const express = require('express');
 const bcrypt = require('bcrypt');
 const crypt = require('crypto');
 const moment = require('moment');
-
-/* utils */
-
 const patternMatcher = require('../utils/patternMatcher');
 const jwt = require('../utils/jwt');
-const {sendVerificationCode} = require('../utils/mailer');
-
-/* middleware */
-
+const { sendVerificationCode } = require('../utils/mailer');
 const tokenVerifier = require('../middleware/tokenVerifier');
-
-/* models */
-
+const bodyVerifier = require('../middleware/bodyVerifier');
 const User = require('../models/user');
 
-// == routes == //
-
-/* setup */
-
 const saltRounds = 8;
-
-/* routes */
-
 const authRouter = express.Router();
 
 /**
  * registers a new user
  */
-authRouter.post('/register', async (request, response) => {
-  const body = request.body;
+authRouter.post('/register',
+  bodyVerifier(['email', 'firstName', 'lastName', 'username', 'password']),
+  async (request, response) => {
+    const body = request.body;
 
-  // verify if body contains necessary info
+    /* check if email is valid */
 
-  if (!body || !('email' in body && 'firstName' in body && 'lastName' in body && 'username' in body && 'password' in body)) {
-    response.status(400).send({ error: 'content missing' });
-    return;
-  }
-
-  // verify valid email
-
-  if (!patternMatcher.isEmailValid(body.email)) {
-    response.status(400).send({ error: 'invalid email' });
-    return;
-  }
-
-  // generate password hash and user model
-
-  const passwordHash = await bcrypt.hash(body.password, saltRounds);
-  const user = new User({
-    firstName: body.firstName,
-    lastName: body.lastName,
-    username: body.username,
-    email: body.email,
-    verification: {
-      code: crypt.randomInt(99999999).toString().padStart(8, '0'),
-      expires: moment(new Date()).add(12, 'hours'),
-      verified: false
-    },
-    passwordHash,
-  });
-
-  try {
-
-    // save
-
-    const res = await user.save();
-    response.status(201).send({ token: jwt.signToken(res.username, res.email, res._id) });
-
-    // send verification email
-
-    const res2 = await sendVerificationCode(res.email, res.verification.code);
-    console.log(res2);
-
-  } catch (e) {                         // email and username are not unique
-
-    if (e.message.startsWith('User validation failed')) {
-      response.status(400).send(e.message);
+    if (!patternMatcher.isEmailValid(body.email)) {
+      response.status(400).send({ error: 'invalid email' });
+      return;
     }
-    throw (e);
-  }
 
+    // generate password hash and user model
 
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
+    const user = new User({
+      firstName: body.firstName,
+      lastName: body.lastName,
+      username: body.username,
+      email: body.email,
+      verification: {
+        code: crypt.randomInt(99999999).toString().padStart(8, '0'),
+        expires: moment(new Date()).add(12, 'hours'),
+        verified: false
+      },
+      passwordHash,
+    });
 
-});
+    return user
+      .save()
+      .then((res) => response.status(201).send({ token: jwt.signToken(res.username, res.email, res._id) }))
+      .catch((err) => response.status(400).send(err.message))
+      .then(() => sendVerificationCode(user.email, user.verification.code));
+  });
 
 /**
  * logs a user in
  */
-authRouter.post('/login', async (request, response) => {
-  const body = request.body;
+authRouter.post('/login',
+  bodyVerifier(['username', 'password']),
+  async (request, response) => {
+    const body = request.body;
 
-  // verify request
+    /* get user */
 
-  if (!body || !('username' in body && 'password' in body)) {
-    response.status(400).send({ error: 'content missing '});
-    return;
-  }
+    const user = await User.findOne().where({ username: body.username });
+    if (!user) {
+      response.status(401).send({ error: 'wrong username or password' });
+      return;
+    }
 
-  // get user
+    /* check password */
 
-  const user = await User.findOne().where({username: body.username});
+    const valid = await bcrypt.compare(body.password, user.passwordHash);
+    if (!valid) {
+      response.status(401).send({ error: 'wrong username or password' });
+      return;
+    }
 
-  if (!user) {
-    response.status(401).send({ error: 'wrong username or password'});
-    return;
-  }
+    /* create token */
 
-  // verify password
-
-  const valid = await bcrypt.compare(body.password, user.passwordHash);
-
-  if (!valid) {
-    response.status(401).send({ error: 'wrong username or password' });
-    return;
-  }
-
-  // generate and return token
-
-  const token = jwt.signToken(user.username, user.email, user._id);
-  response.status(200).send({
-    username: user.username,
-    verified: user.verification.verified,
-    token
+    const token = jwt.signToken(user.username, user.email, user._id);
+    response.status(200).send({
+      username: user.username,
+      verified: user.verification.verified,
+      token
+    });
   });
-});
 
 /**
  * verifies if the user has a valid token
  */
-authRouter.use('/verify', tokenVerifier);
+authRouter.use(tokenVerifier);
+
+/**
+ * verifies if the user has a valid token
+ */
+authRouter.post('/check', async (request, response) => {
+  return response.status(204).send();
+});
 
 /**
  * verifies a user
  */
-authRouter.post('/verify', async (request, response) => {
-  const decToken = jwt.decode(request.get('authorization'));
-  const body = request.body;
-  const user = await User.findById(decToken.id);
+authRouter.post('/verify',
+  bodyVerifier(['code']),
+  async (request, response) => {
+    const body = request.body;
+    const user = request.user;
 
-  if (!body || !('code' in body)) {
-    response.status(400).end();
-    return;
-  }
-
-  if (body.code !== user.verification.code) {
-    response.status(403).send({ error: 'invalid code' });
-    return;
-  }
-
-  if (moment(new Date()).isAfter(user.verification.expires)) {
-    response.status(403).send({ error: 'code expired' });
-    return;
-  }
-
-  await User.findByIdAndUpdate(decToken.id, {
-    verification: {
-      verified: true
+    if (body.code !== user.verification.code) {
+      response.status(403).send({ error: 'invalid code' });
+      return;
     }
-  });
 
-  response.status(200).end();
-});
+    if (moment(new Date()).isAfter(user.verification.expires)) {
+      response.status(403).send({ error: 'code expired' });
+      return;
+    }
+
+    await User.findByIdAndUpdate(user.id, {
+      verification: {
+        verified: true
+      }
+    });
+
+    response.status(200).end();
+  });
 
 /**
  * resends email with verification code
