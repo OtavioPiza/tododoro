8fcd2955186b71e85502113c729e964fcaa9d003
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2022-05-11 23:02:23 -0300

    improved error messages

--- middleware/tokenVerifier.js
+++ middleware/tokenVerifier.js
@@ -11,23 +11,34 @@ const jwt = require('../utils/jwt');
  * @param next
  */
 const tokenVerifier = async (request, response, next) => {
+  
+  /* get and parse token */
+  
   const token = request.get('authorization');
 
   if (!token || !jwt.verifyToken(token)) {
-    response.status(401).end();
+    response.status(401).send({ error: 'Invalid or missing token!' }).end();
     return;
   }
 
   const decoded = jwt.decode(token);
+
+  /* find user associated with that token */
+
   const user = await User.findById(decoded.id);
 
   if (!user) {
-    response.status(401).end();
+    response.status(401).send({ error: 'There is no user associated with that account!' }).end();
     return;
   }
 
-  request.user = user;
+  /* clean up and store user object in request */
 
+  user.id = user._id;
+  delete user._id;
+  delete user.__v;
+
+  request.user = user;
   next();
 };
 
